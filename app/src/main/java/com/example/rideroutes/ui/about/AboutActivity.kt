884213efec721acf14037a6b8248637646951d38
Francisco Lago <franciscolagocervera@gmail.com>
2025-11-26 16:27:38 +0100

    Actualizaciones visuales finales

--- app/src/main/java/com/example/rideroutes/ui/about/AboutActivity.kt
+++ app/src/main/java/com/example/rideroutes/ui/about/AboutActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.rideroutes.R
 
@@ -45,6 +46,7 @@ class AboutActivity : ComponentActivity() {
 
 
     @Composable
+    @Preview
     fun AboutScreen() {
         Surface(
             modifier = Modifier.fillMaxSize(),
