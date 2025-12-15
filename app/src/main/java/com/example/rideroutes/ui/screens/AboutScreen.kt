e2d5c9ce9910742a049446c360dea4f6ceea6310
Francisco Lago <franciscolagocervera@gmail.com>
2025-12-15 22:51:47 +0100

    Práctica 2. Esqueleto para Proximas practicas

--- app/src/main/java/com/example/rideroutes/ui/screens/AboutScreen.kt
+++ app/src/main/java/com/example/rideroutes/ui/screens/AboutScreen.kt
@@ -27,10 +27,12 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.rideroutes.R
 
 @Composable
+@Preview
 fun AboutScreen() {
     val context = LocalContext.current
     Surface(
